51b7415d9ffd8184a488052d5f834af8c77c713c
Drew Martin <drew.e.martin@gmail.com>
2014-08-20 13:45:32 -0400

    added welcomes controller w/ index view

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,15 @@ Rails.application.routes.draw do
 
   resources :comments
 
-  resources :blog_articles
+  resources :blog_articles do
+    resources :comments
+  end
 
-root :to => 'users#index'
-resources :user_sessions
-resources :users
+  root :to => 'users#index'
+  resources :user_sessions
+  resources :users
 
-get 'login' => 'user_sessions#new', :as => :login
-post 'logout' => 'user_sessions#destroy', :as => :logout
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
 
 end
