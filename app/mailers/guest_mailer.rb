7a4a48b57990f64b9236db4a86ed01fb8ee5fa8b
Drew Martin <drew.e.martin@gmail.com>
2014-08-21 17:29:17 -0400

    set up mailer model

--- app/mailers/guest_mailer.rb
+++ app/mailers/guest_mailer.rb
@@ -1,3 +1,12 @@
 class GuestMailer < ActionMailer::Base
   default from: "from@example.com"
+
+  def send_to_admin(mail)
+    @mail = mail
+
+    mail(to: 'drew.e.martin@gmail.com', 
+      subject: "#{@mail.name} emailed you 
+      from your site")
+  end
+
 end
