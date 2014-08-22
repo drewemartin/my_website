a6ce756c6b179ff2928af1c359bd3e56cab95638
Drew Martin <drew.e.martin@gmail.com>
2014-08-22 11:45:44 -0400

    added validations to letter model

--- app/models/letter.rb
+++ app/models/letter.rb
@@ -1,2 +1,35 @@
 class Letter < ActiveRecord::Base
+
+  validates :name, presense: true
+
+  validates :email, presense: true
+  validate :email_must_have_period
+  validate :email_must_have_at_symbol
+
+
+  validates :subject, presense: true
+
+  validates :message, presense: true 
+
+  private
+  def email_must_have_at_symbol
+    unless email.nil?
+
+      if email.split('').include?('@') == false
+        errors.add(:email, 'must include @ symbol')
+      end 
+
+    end  
+  end
+
+  def email_must_have_period
+    unless email.nil?
+
+      if email.split('').include?('.') == false
+        errors.add(:email, 'must include . (period) symbol')      
+      end
+
+    end  
+  end
+
 end
