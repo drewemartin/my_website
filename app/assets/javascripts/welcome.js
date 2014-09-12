6ce9ae38819b2edc7ac1f525c914307bc6e3fad3
Drew Martin <drew.e.martin@gmail.com>
2014-09-12 17:35:36 -0400

    modified view for js handling

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,3 +1,12 @@
 $( document ).ready(function() {
-    
+
+  loadImage();
+
+  function loadImage() {
+    $("#welcomes_pic").effect("slide", { direction: "down" }, 1800); 
+  }
+
+  function loadName(){
+    $('#welcomes_name').css('float', 'right')
+  }
 });
