e266e2f7187a1e190a601b8a5de984ca280a82eb
Drew Martin <drew.e.martin@gmail.com>
2014-09-12 14:45:12 -0400

    js works for alerts and notices

--- app/assets/javascripts/alert_notice.js
+++ app/assets/javascripts/alert_notice.js
@@ -1,1 +1,7 @@
-if($('#alert').text() !== null){console.log('alert_notice is working')}
+$( document ).ready(function() {
+    console.log( "ready!" );
+    if($('#alert').text() !== null || $('#notice').text() !== null){
+      $('#alert_div').delay(3500).slideUp(500);
+      $('#notice_div').delay(3500).slideUp(500);
+    }
+});
