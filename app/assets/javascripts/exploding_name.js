d9f7092ec653912ee101c937a917bcbc5f0a12e2
Drew Martin <drew.e.martin@gmail.com>
2014-09-12 16:06:44 -0400

    refactored code

--- app/assets/javascripts/exploding_name.js
+++ app/assets/javascripts/exploding_name.js
@@ -1,7 +1,7 @@
 $( document ).ready(function(){
   $('#exploding_name').click(function(){
-    $('#exploding_name').css('color', 'red').hide('explode', function(){
-      $('#exploding_name').css('color', 'white')show();
+    $('#exploding_name').css('color', 'red').hide({effect:'explode', pieces: 10, duration: 1000}, function(){
+      $('#exploding_name').css('color', 'white').show();
     })
     
   });
