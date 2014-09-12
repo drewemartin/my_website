57ae5e2ca789d3f0e581f77791f44f2b60382d92
Drew Martin <drew.e.martin@gmail.com>
2014-09-12 15:58:53 -0400

    explode name feauture now works

--- app/assets/javascripts/exploding_name.js
+++ app/assets/javascripts/exploding_name.js
@@ -1,5 +1,10 @@
 $( document ).ready(function(){
-  $("#exploding_name").click(function(){
-    alert('you clicked the right spot')
-  })   
+  $('#exploding_name').click(function(){
+    $('#exploding_name').css('color', 'red').hide('explode', function(){
+      $('#exploding_name').css('color', 'white')show();
+    })
+    
+  });
 });
+
+      
