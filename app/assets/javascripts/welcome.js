dc8092ce768b62fe5a3a20b0c1f5feb998b60de8
Drew Martin <drew.e.martin@gmail.com>
2014-09-13 00:06:33 -0400

    slide js functions work on welcomes#index

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,12 +1,18 @@
 $( document ).ready(function() {
 
   loadImage();
+  loadName();
+
+
 
   function loadImage() {
     $("#welcomes_pic").effect("slide", { direction: "down" }, 1800); 
   }
 
   function loadName(){
-    $('#welcomes_name').css('float', 'right')
+    var distance = ($('#welcomes_name').parent().width() / 2) - ($('#welcomes_name').width() / 2 )
+    $('#welcomes_name').animate({ left: distance }, 1800)
   }
 });
+
+// animate({ left: $(this).width() });
