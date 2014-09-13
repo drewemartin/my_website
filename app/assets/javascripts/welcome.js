94e39afc9784b0b6187467e25efb885d5befa674
Drew Martin <drew.e.martin@gmail.com>
2014-09-13 09:19:42 -0400

    refactored welcome.js so it works w_window resize

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -10,9 +10,16 @@ $( document ).ready(function() {
   }
 
   function loadName(){
+    $('#welcomes_name').parent().attr('align','');
     var distance = ($('#welcomes_name').parent().width() / 2) - ($('#welcomes_name').width() / 2 )
-    $('#welcomes_name').animate({ left: distance }, 1800)
+    $('#welcomes_name').animate({ left: distance }, 1800, function(){
+      console.log('hell0');
+      $('#welcomes_name').parent().attr('align','center');
+      $('#welcomes_name').attr('style', 'left: 0');
+    })
+    
   }
 });
 
 // animate({ left: $(this).width() });
+// 
