b310dc44a7d8383ea20f070039459d97613dd8a6
Drew Martin <drew.e.martin@gmail.com>
2014-09-13 23:47:03 -0400

    removed unneedeed console.logs from js files

--- app/assets/javascripts/alert_notice.js
+++ app/assets/javascripts/alert_notice.js
@@ -1,5 +1,4 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
     if($('#alert').text() !== null || $('#notice').text() !== null){
       $('#alert_div').delay(3500).slideUp(500);
       $('#notice_div').delay(3500).slideUp(500);
--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -2,6 +2,7 @@ $( document ).ready(function() {
 
   loadImage();
   loadName();
+  loadWelcome();
 
 
 
@@ -13,11 +14,18 @@ $( document ).ready(function() {
     $('#welcomes_name').parent().attr('align','');
     var distance = ($('#welcomes_name').parent().width() / 2) - ($('#welcomes_name').width() / 2 )
     $('#welcomes_name').animate({ left: distance }, 1800, function(){
-      console.log('hell0');
       $('#welcomes_name').parent().attr('align','center');
       $('#welcomes_name').attr('style', 'left: 0');
     })
     
+  }
+
+  function loadWelcome(){
+    console.log("__        __     _                               _ ");
+    console.log("\\ \\      / /___ | |  ___  ___   _ __ ___    ___ | |");
+    console.log(" \\ \\ /\\ / // _ \\| | / __|/ _ \\ | '_ ` _ \\  / _ \\| |");
+    console.log("  \\ V  V /|  __/| || (__| (_) || | | | | ||  __/|_|");
+    console.log("   \\_/\\_/  \\___||_| \\___|\\___/ |_| |_| |_| \\___|(_)");
   }
 });
 
