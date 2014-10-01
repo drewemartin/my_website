7c26ed1e2fe460446c1b3105d3ee1d44918b9a4a
Drew Martin <drew.e.martin@gmail.com>
2014-10-01 17:10:47 -0400

    successfully converted DB to postgreSql and imported sqlite data

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140822153021) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "blog_articles", force: true do |t|
     t.text     "body"
     t.string   "title"
@@ -48,6 +51,6 @@ ActiveRecord::Schema.define(version: 20140822153021) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
